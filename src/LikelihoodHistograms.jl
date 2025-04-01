91ae5dd91a76b53215bb0b9980526fb3ef39f82a
oskarhs <oskarhsimensen@gmail.com>
2025-04-01 14:54:55 +0200

    Updated main project file to export histogram_regular and histogram_irregular.

--- src/LikelihoodHistograms.jl
+++ src/LikelihoodHistograms.jl
@@ -1,5 +1,8 @@
 module LikelihoodHistograms
 
-include("histogram.jl")
+export histogram_regular, histogram_irregular
+
+include(joinpath("regular", "histogram_regular.jl"))
+include(joinpath("irregular", "histogram_irregular.jl"))
 
 end
